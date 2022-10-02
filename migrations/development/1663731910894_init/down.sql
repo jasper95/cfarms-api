98930c73ca6bfe083a30b552063d0e29773d8e8e
Jasper Bernales <bernalesjasper@gmail.com>
2022-10-02 17:18:54 +0800

    update initial migrations

---
+++ migrations/development/1663731910894_init/down.sql
@@ -0,0 +1,9 @@
+DROP TABLE public."commodityProduceInventory";
+DROP TABLE public."annualInfo";
+DROP TABLE public."associationToProgram";
+DROP TABLE public."householdToProgram";
+DROP TABLE public."household";
+DROP TABLE public."commodity";
+DROP TABLE public."program";
+DROP TABLE public."farm";
+DROP TABLE public."association";
